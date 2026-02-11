4ce3dcd7293f7cb76e306269bee1f2eb4196adb6
Vamshi Baldewar <vamshibaldewar@gmail.com>
2026-02-11 10:42:31 +0530

    Add recursive binary string generator
    Implement recursive function to generate binary strings without consecutive ones.

---
+++ day_3_Recursion.py
@@ -0,0 +1,11 @@
+# binary string problem using recursion (print all binary string of size n with out consigitive ones)
+
+# def bitstr(n,lastplace=0,string=""):
+#     if n == 0:
+#         print(string)
+#         return 
+#     bitstr(n-1,0,string+"0")
+#     if (lastplace == 0):
+#         bitstr(n-1,1,string+"1")
+        
+# bitstr(3)
