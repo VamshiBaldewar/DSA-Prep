e25b20323846fdf207c3eba3c3a521337894aaf6
Vamshi Baldewar <vamshibaldewar@gmail.com>
2026-02-11 10:54:04 +0530

    Add recursive functions for converting number into words

--- day_3_Recursion.py
+++ day_3_Recursion.py
@@ -11,7 +11,7 @@ def bitstr(n,lastplace=0,string=""):
  bitstr(3)
 
 
-For a given integer array of size N.Youhavetofindalltheoccurrences(indices)ofagivenelement(Key)andprintthem.Usearecursivefunctiontosolvethisproblem.Sample Input: arr[ ] = {3, 2, 4, 5, 6, 2, 7, 2, 2},key = 2Sample Output: 1 5 7 8
+# For a given integer array of size N.Youhavetofindalltheoccurrences(indices)ofagivenelement(Key)andprintthem.Usearecursivefunctiontosolvethisproblem.Sample Input: arr[ ] = {3, 2, 4, 5, 6, 2, 7, 2, 2},key = 2Sample Output: 1 5 7 8
 def findkey(arr,key,res,idx=0):
   n = len(arr)
   if idx == n:
@@ -24,4 +24,17 @@ def findkey(arr,key,res,idx=0):
 arr =[3, 2, 4, 5, 6, 2, 7, 2, 2]
 res= []
 print(findkey(arr,2,res))
-    
+
+# You are given a number (eg -  2019), convert it into a String of english like“two zero one nine”.  Use a recursive function to solve this problem.NOTE-Thedigitsofthenumberwillonlybeintherange0-9andthelastdigitofanumbercan’t be 0
+
+def convert(n):
+  words = ["zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"]
+  if n < 10:
+    return words[n]
+  return convert(n//10)+" "+words[n%10]
+print(convert(2019))
+  
+  
+  
+
+
