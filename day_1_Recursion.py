8ccee8c86b2e2d01b65288a2a1819ca0adf525ef
Vamshi Baldewar <vamshibaldewar@gmail.com>
2026-02-11 11:04:39 +0530

    Uncomment recursive functions in day_1_Recursion.py
    Uncommented the recursive functions and their calls.

--- day_1_Recursion.py
+++ day_1_Recursion.py
@@ -1,16 +1,16 @@
-# def rec(n):
-#     if n == 1:
-#         print(1)
-#         return
-#     print(n)
-#     rec(n-1)
-# rec(10)
-# a=1
-# def increc(n):
-#     global a
-#     if n == 0:
-#         return 0
-#     print(a)
-#     a=a+1
-#     increc(n-1)
-# increc(10)
+def rec(n):
+    if n == 1:
+        print(1)
+        return
+    print(n)
+    rec(n-1)
+rec(10)
+a=1
+def increc(n):
+    global a
+    if n == 0:
+        return 0
+    print(a)
+    a=a+1
+    increc(n-1)
+increc(10)
